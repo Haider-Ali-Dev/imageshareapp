914d405d92e39fe9a76966d2f3710608aa195796
Haider ali <75686258+Haider-Ali-Dev@users.noreply.github.com>
2021-08-21 21:55:42 +0400

    E

--- src/Components/Image/Image.js
+++ src/Components/Image/Image.js
@@ -10,6 +10,8 @@ const  Image = (props) => {
                 <p>Image By: {props.name}</p>
                 <p>Image Description: {props.description}</p>
                 <p>Image Credits: {props.credits}</p>
+                <a href={props.image} download><button className="br4 f6 link dim ph3 pv2 mb2 dib white bg-purple" >Download</button></a>
+                
             </div>
         </div>
 
